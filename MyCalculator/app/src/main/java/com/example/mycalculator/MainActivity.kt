c61992c6664a39d45656209cb4fb958ae201bccc
Andres Haro <hvaandres@icloud.com>
2021-08-15 23:31:45 -0600

    my_calculator

---
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mycalculator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button wworks", Toast.LENGTH_SHORT).show()
+    }
+}
