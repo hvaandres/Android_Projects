22068eaf4f1077be0e54c2f79446d0deebe0025b
Andres Haro <hvaandres@icloud.com>
2021-08-16 05:02:29 -0600

    Adding minus functionality

--- MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,6 +8,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +20,68 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view: View){
 
         tvInput.append((view as Button).text)
-
+        lastNumeric = true
     }
 
     fun onClear(view: View){
 
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
 
+
+    }
+
+    fun onDecimalPoint(view: View){
+        if (lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput.text.toString()
+             var prefix = ""
+
+            try {
+
+                // We check if we have a minus at the beggining
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+
+                }
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    // Check if it is empty or if we have a second minus
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+
+                    tvInput.text = (one.toDouble() + two.toDouble()).toString()
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun onOperator(view: View){
+        if(lastNumeric && !isOperatorAdded(tvInput.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-")){
+           false
+        } else  value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
     }
 }
